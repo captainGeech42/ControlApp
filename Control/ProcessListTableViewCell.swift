74acb3f9cd261fbb753dc3ed0a3954ea4885ddec
Zander Work <zander@sns-imac-2.local>
2016-09-18 22:55:00 -0700

    begun work on process list table view

---
+++ Control/ProcessListTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ProcessListTableViewCell.swift
+//  Control
+//
+//  Created by Zander Work on 9/18/16.
+//  Copyright © 2016 Zander Work. All rights reserved.
+//
+
+import UIKit
+
+class ProcessListTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
