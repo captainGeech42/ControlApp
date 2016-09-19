a16af2c593ce214a9797ab040a6530c3d65f1e33
Zander Work <zander@sns-imac-2.local>
2016-09-18 19:36:35 -0700

    Initial Commit

---
+++ Control/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Control
+//
+//  Created by Zander Work on 9/18/16.
+//  Copyright © 2016 Zander Work. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
